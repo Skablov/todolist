bc36e8d277b22e6cb99e4c5e71dfa0b8e5156f16
Skablov <danilaskablov@list.ru>
2019-04-25 11:50:10 +0300

    Fix work with mongoDB

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const MongoClient = require('mongodb').MongoClient;
+const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('./config');
+
+const mongoClient = new MongoClient(config.MONGO_URL, { useNewUrlParser: true });
+
+
 
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
-var arr = ['hello', 'world', 'test'];
 
-app.get('/', (req, res) => res.render('index', {arr: arr}));
-app.get('/create', (req, res) => res.render('create'));
-app.post('/create', (req, res) => {
-	arr.push(req.body.text);
-	res.redirect('/');
-});
+app.get('/', (req, res) => { // рисуем индекс
+	res.render('index');
+	});
 
-// replace the uri string with your connection string.
-const uri = "mongodb+srv://admin:admin@cluster0-3ogiv.mongodb.net/test?retryWrites=true"
-MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected...');
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
+app.get('/tasks',(req, res) =>{
+	mongoClient.connect((err, client)=>{
+	const db = client.db('todolist');
+	const collection = db.collection('data');
+	
+	collection.find({}).toArray((err, results)=>{
+		let arr = [];
+		let i = 0;
+		while(results[i])
+		{
+			arr.push(results[i]._id);
+			arr.push(results[i].task);
+			i++;
+		}
+		res.send(arr);
+	})
 });
+})
+
+
 
 app.listen(config.PORT, () => console.log(`Server start on ${config.PORT}`));
-
